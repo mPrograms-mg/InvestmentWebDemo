a21ee05fea8112cbce8927d509cb4c4f13a97a97
mPrograms-mg <maheshkshirsagar128@gmail.com>
2023-06-26 22:38:05 +0530

    Add Auth page and book page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { NgImageSliderModule } from 'ng-image-slider';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { PortfolioServicesComponent } from './components/portfolio-services/portfolio-services.component';
 import { ScrollComponent } from './components/scroll/scroll.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { RegistrationPageComponent } from './auth/registration-page/registration-page.component';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { BookPageComponent } from './pages/book-page/book-page.component';
 @NgModule({
-  declarations: [AppComponent, CarouselComponent, PortfolioServicesComponent, ScrollComponent],
+  declarations: [AppComponent, CarouselComponent, PortfolioServicesComponent, ScrollComponent, ContactPageComponent, AboutPageComponent, RegistrationPageComponent, LoginPageComponent, BookPageComponent],
   imports: [BrowserModule, AppRoutingModule, LayoutModule, NgImageSliderModule],
   providers: [],
   bootstrap: [AppComponent],
